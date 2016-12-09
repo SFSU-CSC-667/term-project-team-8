8ffde55d01a77146233cee012aa372f2411801e9
Arjun <arjunpatel422@gmail.com>
2016-12-08 22:44:22 -0800

    Login and Registration Implemented
    The login and registration functions now use encryption and perform
    appropiate behavior.

--- cahteam8server/routes/model.js
+++ cahteam8server/routes/model.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const crypto = require ('crypto');
 const router = express.Router();
 const pgp = require('pg-promise')();
 const DATABASE_URL = process.env.DATABASE_URL || "postgres://qgjmjrvpnooxbd:-kllbSyNCkgI7aLHRg0RxlAOjv@ec2-23-23-222-147.compute-1.amazonaws.com:5432/d5kk4m3ru0nl0c?ssl=true";
+
 console.log(DATABASE_URL);
 const db = pgp(DATABASE_URL);
+
 
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
@@ -14,64 +17,73 @@ router.get('/hand',function(req,res,next) {
 res.render('hand');
 });
 
-router.get('/data',function getData(req,res,next) {
-const result = new Object();
-db.any("select * from items") 
- .then(function(data) {
-     console.log("DATA:",data);
-     return res.send(data);
+router.get('/lobby',function(req,res,next) {
+res.render('lobby');
+});
+
+router.post('/login',function(req,res)
+{
+   const email = req.body.email;
+   const password = new Buffer(req.body.password).toString('base64');
+   if(email == "")
+     res.redirect('/');
+   else
+   {
+     const query = `select * from player where email = \'${email}\'  AND password = \'${password}\'`;
+     console.log(query);
+     db.any(query) .then(function(data) {
+     if(data.length == 0)
+        res.redirect('/');
+     else
+        res.redirect('/lobby');
   })
   .catch(function(error) {
      console.log("ERROR:",error);
      return res.send(error);
   })
-});
-
-router.get('/login',function (req, res)
-{
-  res.render('login');
-  //res.send(data.data);
-});
-
-router.get('/register',function (req, res)
-{
-  res.render('login');
-  //res.send(data.data);
-});
-
-
-router.post('/login',function(req,res)
-{
-   res.json({
-      email:req.body.email,
-      password:req.body.password
-   })
+ 
+   }
 });
 
 router.post('/register',function(req,res)
 {
    const username = req.body.email;
-   const password = req.body.password;
+   const password = new Buffer(req.body.password).toString('base64');
    const email = req.body.email;
-   const queryString = `INSERT INTO player (username,password,email) VALUES (\'${username}\','${password}\',\'${email}\')`;
- console.log(queryString);  
- db.none(queryString,[true])
- .then(function() {
-     console.log("QueryString:",queryString);
-     return res.json({
-      email:req.body.email,
-      password:req.body.password
-   })
+   if(username == "" || password == "" || email =="")
+      res.redirect('/');
+   else
+   {
+       const checkQuery = `select * from player where email = \'${email}\'  OR username = \'${username}\'`;
+       console.log(checkQuery);
+       db.any(checkQuery) .then(function(data) {
+       if(data.length == 0)
+       {
+         const insertQuery = `INSERT INTO player (username,password,email) VALUES (\'${username}\','${password}\',\'${email}\')`;
+         console.log(insertQuery);
+        db.none(insertQuery,[true])
+        .then(function() {
+        res.redirect('/lobby');
+        })
+        .catch(function(error) {
+        console.log("ERROR:",error);
+        return res.send(error);
+        })
+      }
+     else
+        res.redirect('/');
   })
   .catch(function(error) {
      console.log("ERROR:",error);
      return res.send(error);
   })
+         
+   }
 });
 
 router.get('/',function(req,res)
 {
-  res.send('Success');
+  res.render('login');
 });
 
 module.exports = router;
